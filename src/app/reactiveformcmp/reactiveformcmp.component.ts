2352b3a2815e9a03e6eb0190120e46a7afdaffcc
RishiSetpal <rishisetpal123@gmail.com>
2025-02-09 16:46:53 +0530

    L4 P3 Using list of Validators Use Regex too, ?.invalid ?.touched ?.error.['required'] ?.error.['pattern']
    09-02-2025

--- src/app/reactiveformcmp/reactiveformcmp.component.ts
+++ src/app/reactiveformcmp/reactiveformcmp.component.ts
@@ -9,14 +9,23 @@ import {
 
 /**
  * L4 P2 Made use of Validators & <div *ngIf = loginForm.get('username')?.invalid && loginForm.get('username')?.touched style="color: red;">
- * CommonModule for *ngIf
- * 09-02-2025
- */
+  CommonModule for *ngIf
+  09-02-2025
+ * L4 P3 Using list of Validators
+  Use Regex too, 
+  ?.invalid ?.touched 
+  ?.error.['required']
+  ?.error.['pattern']
+  09-02-2025 
+ * 
+
+  */
 @Component({
   selector: 'app-reactiveformcmp',
   // standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
+    ReactiveformcmpComponent
     <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
       <input type="text" formControlName="username" placeholder="username" />
       <div
@@ -26,8 +35,32 @@ import {
         "
         style="color: red;"
       >
-        UserName is Required!
+        <span *ngIf="loginForm.get('username')?.errors?.['required']">
+          UserName is Required
+        </span>
+        <span *ngIf="loginForm.get('username')?.errors?.['pattern']">
+          Invalid UserName
+        </span>
       </div>
+      <br />
+
+      <br />
+      <input type="text" formControlName="phone" />
+      <div
+        *ngIf="
+          loginForm.get('phone')?.invalid && loginForm.get('phone')?.touched
+        "
+        style="color: red;"
+      >
+        <span *ngIf="loginForm.get('phone')?.errors?.['required']">
+          Phone is Required
+        </span>
+        <span *ngIf="loginForm.get('phone')?.errors?.['pattern']">
+          Invalid Phone
+        </span>
+      </div>
+      <br />
+      <br />
       <button type="submit">Submit</button>
     </form>
   `,
@@ -35,10 +68,21 @@ import {
 })
 export class ReactiveformcmpComponent {
   loginForm = new FormGroup({
-    username: new FormControl('', Validators.required),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[A-Za-z]{3,20}$/),
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d{10}$/),
+    ]),
   });
 
   onSubmit = () => {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
     console.log('onSubmit:', this.loginForm.value);
   };
 }
