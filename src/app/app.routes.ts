9598349ac6561cb28f03aa7436afda398e29c2ce
RishiSetpal <rishisetpal123@gmail.com>
2025-02-23 16:04:18 +0530

    L6 P2 Add Default Router 23-02-2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CounterComponent } from './counter/counter.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  // { path: '', component: AppComponent },
+  { path: '', redirectTo: 'counter', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'counter', component: CounterComponent },
   { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  imports: [],
+  template: `
+    <p>
+      page-not-found works!
+      Go back to home page and try again!
+    </p>
+  `,
+  styles: ``
+})
+export class PageNotFoundComponent {
+
+}
